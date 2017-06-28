27ba3efeaaa634f7a27b33e69d7e73e6f9c54aa8
Brahima Traore <kounandi@Mac-mini-de-Brahima.local>
2017-06-28 17:43:33 -0400

    Add Task Collections

---
+++ imports/ui/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.jsx';
+
+// App component - represents the whole app
+class App extends Component {
+
+  /*  getTasks() {
+    return [
+      { _id: 1, text: 'This is task 1' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
+    ];
+  }
+
+  renderTasks() {
+    return this.getTasks().map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  } */
+ 
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+//The container fetches tasks from the Tasks
+// collection and supplies them to the underlying App component 
+//it wraps as the tasks prop. It does this in a reactive way,
+// so that when the contents of the database change, 
+//the App re-renders, as we'll soon see!
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
