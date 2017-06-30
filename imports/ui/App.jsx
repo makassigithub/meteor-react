9466dfab9704ba54551705c8c64e83465ca82d89
Brahima Traore <mksi@MacBook-Pro-de-Brahima.local>
2017-06-29 22:09:46 -0400

    modifying sate using setState

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; 
 import ReactDOM from 'react-dom';
 
 import { Tasks } from '../api/tasks.js';
@@ -9,6 +9,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    }
+
+}
 
 handleSubmit(event) {
     event.preventDefault();
@@ -25,8 +33,22 @@ handleSubmit(event) {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+   //When task is clicked, we change the completed status to the opposit on the state
+    toggleHideCompleted(){
+      this.setState({
+        hideCompleted:!this.state.hideCompleted,
+      });
+    }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+
+    // get all tasks from the state and filter them to return those not completed
+    let filteredTasks = this.props.tasks;
+    if(this.state.hideCompleted){
+      filteredTasks = filteredTasks.filter(tasks => !tasks.checked);
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -36,6 +58,15 @@ handleSubmit(event) {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <label className="hide-completed">
+            <input 
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+          />
+          Hide Completed tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
